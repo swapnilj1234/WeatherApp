d4dc811463f277751413895ab82ddece8bbf4144
swapnil <swap81733@gmail.com>
2020-11-09 16:07:53 +0530

    Basic Design for Two ViewController Convert All in MVC Pattern

---
+++ 13.WeatherApp/Controller/CityViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CityViewController.swift
+//  13.WeatherApp
+//
+//  Created by swapnil jadhav on 09/11/20.
+//  Copyright © 2020 t. All rights reserved.
+//
+
+import UIKit
+
+class CityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+  
+}
---
+++ 13.WeatherApp/Model/WeatherDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherDataModel.swift
+//  13.WeatherApp
+//
+//  Created by swapnil jadhav on 09/11/20.
+//  Copyright © 2020 t. All rights reserved.
+//
+
+import Foundation
